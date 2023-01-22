98db3000de76536f33da6e5415446a79a5d9a780
KoketsuRyuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-01-22 11:50:32 +0000

    [Add]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @user = current_user
   end
 
   def show
@@ -9,7 +10,19 @@ class UsersController < ApplicationController
   end
   
   def edit
-    
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user = User.update(user_params)
+    redirect_to user_path(user.id)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:profile_image,:introduction)
   end
   
 end
